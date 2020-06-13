0dd54caf803f77ff75d515b08e0bffefe8a9245a
maklaen <maklaen@gmail.com>
2020-06-13 14:46:32 +0700

    First commit

--- src/package_java/hierarchy/AbstractAudio.java
+++ src/package_java/hierarchy/AbstractAudio.java
@@ -1,4 +1,25 @@
 package package_java.hierarchy;
 
-public class AbstractAudio {
+public abstract class AbstractAudio {
+    private String name;
+    private int price;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void printPrice(){
+        System.out.println("Цена: " +getPrice());
+    }
+    public String description() {
+        return "Модель: " + name;
+    }
+    public void printDescription()
+    {
+        System.out.println(description());
+    }
 }
